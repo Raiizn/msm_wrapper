57bf1fb58c6b93655e2027c8ca1d6af76e809202
Raiizn <Tornadoguy712@hotmail.com>
2021-10-18 16:29:08 -0400

    Basic API routes for ajax pages.

--- msm_wrapper.py
+++ msm_wrapper.py
@@ -3,7 +3,7 @@ HTML Wrapper server for MSM management.
  MSM Link: https://msmhq.com/
  To install as a service in linux, use systemctl. A service file template is provided.
 """
-
+import json
 import logging
 import urllib.request
 from http.server import SimpleHTTPRequestHandler
@@ -13,6 +13,7 @@ import socketserver
 from typing import Tuple, Union
 import server_pinger
 
+IP = ""
 AUTH_KEY = ""
 CONFIG = None
 SERVER = None
@@ -63,9 +64,9 @@ def run_command(command_args):
 
 def get_server_info() -> Tuple[bool, Union[server_pinger.Server, None]]:
     result = run_command(["msm", SERVER, "status"])
-    if not result:  # MSM not running
-        return (False, None)
-    if "is running" not in result: # MSM says it's down
+    # if not result:  # MSM not running
+    #    return (False, None)
+    if result is None or "is running" not in result: # MSM says it's down
         return (False, None)
 
     # MSM says the server is running so grab the info from the pinger.
@@ -101,15 +102,14 @@ class AuthHandler(SimpleHTTPRequestHandler):
         max_players = server.players.max if online and server.accepting_connections else 0
 
         domain_status = "NO_SYNC"
-        IP = urllib.request.urlopen('https://ident.me').read().decode('utf8')
         values = template.replace("{{IP}}", IP).replace("{{DOMAIN}}", CONFIG["domain"])
         values = values.replace("{{STATUS}}", status).replace("{{DOMAIN_STATUS}}", domain_status)
         values = values.replace("{{PLAYERS}}", str(players)).replace("{{MAX_PLAYERS}}", str(max_players))
         return values
 
-    def do_HEAD(self):
+    def do_HEAD(self, content_type='text/html'):
         self.send_response(200)
-        self.send_header('Content-type', 'text/html')
+        self.send_header('Content-type', content_type)
         self.end_headers()
 
     def do_AUTHHEAD(self):
@@ -117,7 +117,34 @@ class AuthHandler(SimpleHTTPRequestHandler):
         self.send_header('WWW-Authenticate', 'Basic realm=\Administration\"')
         self.send_header('Content-type', 'text/html')
         self.end_headers()
+        
+    def get_API(self, args):
+        self.do_HEAD('application/json')
+        if args in self.api_routes:
+            contents = self.api_routes[args](self)
+            if isinstance(contents, str):
+                contents = {"value": contents}
+            self.wfile.write(bytes(json.dumps(contents), 'utf-8'))
+        self.end_headers()
 
+    ## API Methods ##
+
+    def get_status(self):
+        online, server = get_server_info()
+        return get_server_status((online, server))
+
+    def get_players(self):
+        online, server = get_server_info()
+        result = {"online": 0, "max": 0}
+        if online:
+            result = {"online": server.players.online,
+                      "max": server.players.max}
+        return result
+
+    def get_ip(self):
+        return IP
+
+    ## Get processor and router
     def do_GET(self):
         ''' Present frontpage with user authentication. '''
         logging.info(self.path)
@@ -132,6 +159,9 @@ class AuthHandler(SimpleHTTPRequestHandler):
             else:
                 self.do_AUTHHEAD()
                 self.wfile.write(self.error_bytes("Forbidden", "Not authorized."))
+        elif self.path.startswith("/api"):
+            self.get_API(self.path[4:])
+            
         else:
             if self.path in CONFIG["permitted-resources"]:
                 super().do_GET()
@@ -147,6 +177,12 @@ class AuthHandler(SimpleHTTPRequestHandler):
         self.end_headers()
         self.wfile.write(self.error_bytes("Not Allowed", "Request type not allowed."))
 
+    api_routes = {"/status": get_status,
+                 "/players": get_players,
+                 "/ip": get_ip}
+
+
+
 if __name__ == "__main__":
     logging.info("Reading config...")
     config=read_config()
@@ -156,6 +192,9 @@ if __name__ == "__main__":
     print("Done!")
     print(str(config))
     print(template)
+
+    # Get IP from external API
+    IP = urllib.request.urlopen('https://ident.me').read().decode('utf8')
 
     # Set up socket server
     AUTH_KEY = b64encode(bytes(f"{config['admin-name']}:{config['admin-pass']}", "utf-8")).decode("ISO-8859-1")
